b0134b93458783477a06aa63ed279dc5ed25eecf
Vishal Bhatia <124044416+Vishal-Kaku@users.noreply.github.com>
2023-01-31 18:40:34 +0530

    Updated Factorial Code

--- RECURTION/factorial.cpp
+++ RECURTION/factorial.cpp
@@ -1,15 +1,17 @@
 #include<stdio.h>
-long int factorial(int n);
+
+long int factorial(int n){
+    if (n>=1)
+        return n*factorial(n-1);
+    else
+        return 1;
+}
+
+
 int main(){
     int n;
     printf("Enter your number = ");
     scanf("%d",&n);
     printf("Factorial of %d = %d",n, factorial(n));
     return 0;
-}
-long int factorial(int n){
-    if (n>=1)
-        return n*factorial(n-1);
-    else
-        return 1;
 }
