ed6843788cd19e543e63094c8fe5c3d97f98b29f
Vishal Bhatia <124044416+Vishal-Kaku@users.noreply.github.com>
2023-02-16 16:02:36 +0530

    Update Prime_Number.cpp

--- FINAL CODE/Prime_Number.cpp
+++ FINAL CODE/Prime_Number.cpp
@@ -1,23 +1,23 @@
 #include<stdio.h> 
 int main(){
     int i,n;
-    printf("enter the number to check prime = ");
+    printf("enter the number to check even = ");
     scanf("%d",&n);     
-   bool isPrime= true;
+   bool iseven= true;
         for(i=2;i<=n-1;i++){
         int rem;
         rem=n%i;
         if(rem == 0){
-            isPrime= false;
+            iseven= false;
             break;
         }
             
 
     }
 
-    if(isPrime == false){
-            printf("number is prime");
+    if(iseven == false){
+            printf("number is even");
     }else
-            printf("number is not prime");
+            printf("number is not even");
         return 0;
 }
